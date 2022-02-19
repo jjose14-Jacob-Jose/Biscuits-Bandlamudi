0978d0422810ec91194dc53d141e0184bbf734a4
SantoshKaluva1 <90415238+SantoshKaluva1@users.noreply.github.com>
2022-02-18 21:51:18 -0700

    jsonParse code to write in another json file

--- main/java/com/biscuit/models/jsonReader.java
+++ main/java/com/biscuit/models/jsonReader.java
@@ -34,35 +34,35 @@ public class jsonReader {
 	            System.out.println(iterator3.next());
 	         }
 	         
+		      //Inserting key-value pairs into the json object
+		      jsonObject.put("names", "name");
+		      jsonObject.put("description", "description");
+		      jsonObject.put("backlog", backlog);
+		      jsonObject.put("releases", releases);
+		      jsonObject.put("sprints", sprints);
+		      try {
+		         FileWriter file = new FileWriter("/Users/santoshkaluva/biscuit/sample_data.json");
+		         file.write(jsonObject.toJSONString());
+		         file.close();
+		      } catch (IOException e) {
+		         // TODO Auto-generated catch block
+		         e.printStackTrace();
+		      }
+		      System.out.println("JSON file created: "+jsonObject);
+		
+		
 	         
 	      } catch(Exception e) {
 	         e.printStackTrace();
 	      }
 	      
+	      
+	      
 	     
 	
 	}
 	
-//	public void writeFile()
-//	{
-//		 JSONObject jsonObject = new JSONObject();
-//	      //Inserting key-value pairs into the json object
-//	      jsonObject.put("names", "name");
-//	      jsonObject.put("description", "description");
-//	      jsonObject.put("backlog", backlog);
-//	      jsonObject.put("Date_Of_Birth", "1981-12-05");
-//	      jsonObject.put("Place_Of_Birth", "Delhi");
-//	      jsonObject.put("Country", "India");
-//	      try {
-//	         FileWriter file = new FileWriter("E:/output.json");
-//	         file.write(jsonObject.toJSONString());
-//	         file.close();
-//	      } catch (IOException e) {
-//	         // TODO Auto-generated catch block
-//	         e.printStackTrace();
-//	      }
-//	      System.out.println("JSON file created: "+jsonObject);
-//	}
+}
 	
 
 }
