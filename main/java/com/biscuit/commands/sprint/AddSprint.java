c728302661316a1f2a0fd15fc6d9f2150c3354d5
Srikar Vodeti <svodeti@asu.edu>
2022-02-25 16:17:26 -0700

    updated gitignore

--- main/java/com/biscuit/commands/sprint/AddSprint.java
+++ main/java/com/biscuit/commands/sprint/AddSprint.java
@@ -10,11 +10,16 @@ import com.biscuit.commands.Command;
 import com.biscuit.factories.DateCompleter;
 import com.biscuit.models.Project;
 import com.biscuit.models.Sprint;
+import com.biscuit.models.enums.SprintReviewStatus;
 import com.biscuit.models.enums.Status;
+import com.biscuit.models.enums.StatusSprint;
 
 import jline.console.ConsoleReader;
 import jline.console.completer.AggregateCompleter;
+import jline.console.completer.ArgumentCompleter;
 import jline.console.completer.Completer;
+import jline.console.completer.NullCompleter;
+import jline.console.completer.StringsCompleter;
 
 public class AddSprint implements Command {
 
@@ -32,12 +37,14 @@ public class AddSprint implements Command {
 
 	public boolean execute() throws IOException {
 		StringBuilder description = new StringBuilder();
+
 		String prompt = reader.getPrompt();
 
 		sprint.project = project;
 		setName();
 
 		setDescription(description);
+		setPlanning();
 
 		sprint.state = Status.CREATED;
 		sprint.startDate = new Date(0);
@@ -51,6 +58,7 @@ public class AddSprint implements Command {
 
 		sprint.assignedEffort = 0;
 		setVelocity();
+		setReview();
 
 		reader.setPrompt(prompt);
 
@@ -62,7 +70,87 @@ public class AddSprint implements Command {
 
 		return false;
 	}
+	
+	private void setPlanning() throws IOException {
+		// List<String> businessValues = new ArrayList<String>();
+		String line;
+		Completer oldCompleter = (Completer) reader.getCompleters().toArray()[0];
 
+		// for (BusinessValue bv : BusinessValue.values()) {
+		// businessValues.add(bv.name().toLowerCase());
+		// }
+
+		Completer businessValuesCompleter = new ArgumentCompleter(new StringsCompleter(StatusSprint.values), new NullCompleter());
+
+		reader.removeCompleter(oldCompleter);
+		reader.addCompleter(businessValuesCompleter);
+
+		reader.setPrompt(ColorCodes.BLUE + "\nSprint Planning:\n" + ColorCodes.YELLOW + "(hit Tab to see valid values)\n" + ColorCodes.RESET);
+
+		while ((line = reader.readLine()) != null) {
+			line = line.trim().toUpperCase();
+
+			try {
+				sprint.planning = StatusSprint.valueOf(line);
+			} catch (IllegalArgumentException e) {
+				System.out.println(ColorCodes.RED + "invalid value" + ColorCodes.RESET);
+				continue;
+			}
+
+			reader.removeCompleter(businessValuesCompleter);
+			reader.addCompleter(oldCompleter);
+			break;
+		}
+		
+		StringBuilder planningNote = new StringBuilder();
+		StringBuilder sprintGoals = new StringBuilder();
+		if((sprint.planning)==StatusSprint.PLANNED)
+		{
+			setSprintPLanDescription(planningNote);
+			setSprintGoals(sprintGoals);
+		}
+
+	}
+
+	private void setReview() throws IOException {
+		// List<String> businessValues = new ArrayList<String>();
+		String line;
+		Completer oldCompleter = (Completer) reader.getCompleters().toArray()[0];
+
+		// for (BusinessValue bv : BusinessValue.values()) {
+		// businessValues.add(bv.name().toLowerCase());
+		// }
+
+		Completer businessValuesCompleter = new ArgumentCompleter(new StringsCompleter(SprintReviewStatus.values), new NullCompleter());
+
+		reader.removeCompleter(oldCompleter);
+		reader.addCompleter(businessValuesCompleter);
+
+		reader.setPrompt(ColorCodes.BLUE + "\nSprint Review:\n" + ColorCodes.YELLOW + "(hit Tab to see valid values)\n" + ColorCodes.RESET);
+
+		while ((line = reader.readLine()) != null) {
+			line = line.trim().toUpperCase();
+
+			try {
+				sprint.review = SprintReviewStatus.valueOf(line);
+			} catch (IllegalArgumentException e) {
+				System.out.println(ColorCodes.RED + "invalid value" + ColorCodes.RESET);
+				continue;
+			}
+
+			reader.removeCompleter(businessValuesCompleter);
+			reader.addCompleter(oldCompleter);
+			break;
+		}
+		
+		StringBuilder reviewNote = new StringBuilder();
+
+		if((sprint.review)== SprintReviewStatus.COMPLETED)
+		{
+			setSprintReviewDescription(reviewNote);
+		}
+
+	}
 
 	private void setDueDate() throws IOException {
 		String line;
@@ -234,6 +322,51 @@ public class AddSprint implements Command {
 		}
 
 		sprint.description = description.toString();
+	}
+	
+	private void setSprintPLanDescription(StringBuilder planningNote) throws IOException {
+		String line;
+		reader.setPrompt(ColorCodes.BLUE + "\nNote from Planning Session:\n" + ColorCodes.YELLOW + "(\\q to end writing)\n" + ColorCodes.RESET);
+
+		while ((line = reader.readLine()) != null) {
+			if (line.equals("\\q")) {
+				break;
+			}
+			planningNote.append(line).append("\n");
+			reader.setPrompt("");
+		}
+
+		sprint.planningNote = planningNote.toString();
+	}
+	
+	private void setSprintReviewDescription(StringBuilder reviewNote) throws IOException {
+		String line;
+		reader.setPrompt(ColorCodes.BLUE + "\nNote from Review Session:\n" + ColorCodes.YELLOW + "(\\q to end writing)\n" + ColorCodes.RESET);
+
+		while ((line = reader.readLine()) != null) {
+			if (line.equals("\\q")) {
+				break;
+			}
+			reviewNote.append(line).append("\n");
+			reader.setPrompt("");
+		}
+
+		sprint.reviewNote = reviewNote.toString();
+	}
+	
+	private void setSprintGoals(StringBuilder sprintGoals) throws IOException {
+		String line;
+		reader.setPrompt(ColorCodes.BLUE + "\nSprinGoals:\n" + ColorCodes.YELLOW + "(\\q to end writing)\n" + ColorCodes.RESET);
+
+		while ((line = reader.readLine()) != null) {
+			if (line.equals("\\q")) {
+				break;
+			}
+			sprintGoals.append(line).append("\n");
+			reader.setPrompt("");
+		}
+
+		sprint.sprintGoals = sprintGoals.toString();
 	}
 
 
